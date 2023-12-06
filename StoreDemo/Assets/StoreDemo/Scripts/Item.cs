fea3d8ef943c76234843db811031b6a61c27d44c
Jvbs0504 <joaovbs0504@gmail.com>
2023-12-05 21:54:34 -0300

    Added Item Variations

--- StoreDemo/Assets/StoreDemo/Scripts/Item.cs
+++ StoreDemo/Assets/StoreDemo/Scripts/Item.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 public class Item : ScriptableObject
 {
     public Type m_type;
+    public EquipType m_equipType;
     public string m_name;
     public Sprite m_icon;
     public float m_price;
@@ -14,4 +15,12 @@ public enum Type
     Equipable,
     Consumable,
     Coin
+}
+public enum EquipType
+{
+    None,
+    Face,
+    Head,
+    Body,
+    Weapons,
 }
